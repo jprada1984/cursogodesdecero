52d3ce90c40eea89c271eb5f577338cd814f8586
Jimmy Prada Guadalupe <jimmy.prada@externos.bci.cl>
2024-08-18 15:52:01 -0500

    siguiente commit

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/jprada1984/cursogodesdecero/variables"
+import (
+	"fmt"
+
+	"github.com/jprada1984/cursogodesdecero/variables"
+)
 
 func main() {
-	variables.MostrarEntero()
+	// variables.MostrarEntero()
+	correcto, valor := variables.ConviertoaTexto(150)
+	fmt.Println(correcto)
+	fmt.Println(valor)
 }
---
+++ variables/resto.go
@@ -0,0 +1,28 @@
+package variables
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+func RestoVariable() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1577.66
+	Fecha = time.Now()
+
+	fmt.Println("Nombre:", Nombre)
+	fmt.Println("Estado:", Estado)
+	fmt.Println("Sueldo:", Sueldo)
+	fmt.Println("Fecha:", Fecha)
+}
+
+func ConviertoaTexto(numero int) (bool, string) {
+	return true, strconv.Itoa(numero)
+}
